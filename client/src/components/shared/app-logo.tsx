bd04632e4a417117cf9a688092b4933d4551d8ab
Warrick Smith <warrick.smith@neo.nz>
2024-08-27 09:21:46 +1200

    feat(client): Update Next.js application configuration
    - Add CORS configuration to allow cross-origin requests
    - Ensure images are correctly loaded by updating their paths to use the `/raceday2/` prefix

---
+++ client/src/components/shared/app-logo.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function AppLogo() {
+  return (
+    <Link href="/" className="flex-start">
+      <div className={`flex flex-row items-end space-x-2`}>
+        <Image
+          src="/raceday2/logo.svg"
+          width={32}
+          height={32}
+          alt={`Raceday 2 logo`}
+          priority
+        />
+        <span className="text-xl">Raceday 2</span>
+      </div>
+    </Link>
+  )
+}
