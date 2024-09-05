d607a28abc66a743069f4d31b34b9744e381f9ff
Warrick Smith <warrick.smith@neo.nz>
2024-09-05 12:08:39 +1200

    feat: add Radix UI components and dashboard meetings view
    ## Description
    
    - Add Radix UI components for scroll area and tabs
    - Implement a dashboard meetings view that fetches and displays today's race meetings
    - Update package dependencies and aliases in the project configuration
    
    ## Related Issue
    
    ## Checklist
    
    - [x] You have tested the changes locally and they work as expected
    - [x] You have added appropriate documentation or updated existing documentation
    - [x] You have followed the coding style and guidelines of the project
    - [x] You have added/updated tests to ensure the changes are properly covered
    - [x] You have reviewed the changes and ensured they meet the project's quality standards
    
    ## Screenshots (if applicable)
    
    ## Additional Notes

--- client/src/components/shared/dashboard/dashboard.tsx
+++ client/src/components/shared/dashboard/dashboard.tsx
@@ -1,11 +1,17 @@
-import { CardsSkeleton } from '../skeletons'
+import { Suspense } from 'react'
+import { CardSkeleton } from '../skeletons'
+import RaceMeetingsWrapper from './race-meetings-wrapper'
 
 const Dashboard = () => {
   return (
     <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>Racing Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <CardsSkeleton />
+        <Suspense fallback={<CardSkeleton />}>
+          <RaceMeetingsWrapper />
+        </Suspense>
+        <CardSkeleton />
+        <CardSkeleton />
       </div>
     </main>
   )
