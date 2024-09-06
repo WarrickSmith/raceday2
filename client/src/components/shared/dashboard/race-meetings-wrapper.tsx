9ae67b23ed9d38ac399e0faa5dce02e18c2c8c51
Warrick Smith <warrick.smith@neo.nz>
2024-09-06 15:54:06 +1200

    feat: update ESLint configuration and race meetings UI
    ## Description
    
    This commit includes the following changes:
    
    - Updated the `.eslintrc.json` file to remove the `next/babel` extension from the ESLint configuration
    - Improved the formatting and layout of the `RaceMeetingsWrapper` component in the `race-meetings-wrapper.tsx` file
    
    These changes aim to enhance the codebase by ensuring a consistent ESLint configuration and improving the visual presentation of the race meetings section in the dashboard.

--- client/src/components/shared/dashboard/race-meetings-wrapper.tsx
+++ client/src/components/shared/dashboard/race-meetings-wrapper.tsx
@@ -10,7 +10,9 @@ const RaceMeetingsWrapper = async () => {
     <Card className="h-[400px] flex flex-col">
       <CardHeader className="flex flex-row items-center space-y-0 pb- gap-2">
         <CalendarFold />
-        <CardTitle className="text-lg font-medium">Today's Meetings</CardTitle>
+        <CardTitle className="text-lg font-medium">
+          {"Today's Meetings"}
+        </CardTitle>
       </CardHeader>
       <CardContent className="flex-grow overflow-hidden">
         <Meetings meetings={meetings} />
