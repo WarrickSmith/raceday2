b5c9e2456053dc643b510c3b9574c6da5118db9d
Warrick Smith <warrick.smith@neo.nz>
2024-09-17 11:31:46 +1200

    feat: add MeetingButton component and update dashboard layout
    ## Description
    
    This change introduces a new `MeetingButton` component to the dashboard, which is used to display a meeting item in the `MeetingsList` component. The `MeetingButton` component handles the styling and behavior of the meeting button, including highlighting the selected meeting.
    
    Additionally, the `dashboard.tsx` file has been updated to use the new `RunnersWrapper` component and improve the layout of the dashboard components.
    
    ## Related Issue
    
    None
    
    ## Checklist
    
    - [x] You have tested the changes locally and they work as expected
    - [x] You have followed the coding style and guidelines of the project
    - [x] You have reviewed the changes and ensured they meet the project's quality standards

--- client/src/components/dashboard/dashboard.tsx
+++ client/src/components/dashboard/dashboard.tsx
@@ -6,23 +6,21 @@ import RunnersWrapper from './runners-wrapper'
 
 export const experimental_ppr = true
 
-const Dashboard = () => {
-  return (
-    <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>Racing Dashboard</h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <Suspense fallback={<CardSkeleton />}>
-          <RaceMeetingsWrapper />
-        </Suspense>
-        <Suspense fallback={<CardSkeleton />}>
-          <RacesWrapper />
-        </Suspense>
-        <Suspense fallback={<CardSkeleton />}>
-          <RunnersWrapper />
-        </Suspense>
-      </div>
-    </main>
-  )
-}
+const Dashboard = () => (
+  <main>
+    <h1 className={`mb-4 text-xl md:text-2xl`}>Racing Dashboard</h1>
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <Suspense fallback={<CardSkeleton />}>
+        <RaceMeetingsWrapper />
+      </Suspense>
+      <Suspense fallback={<CardSkeleton />}>
+        <RacesWrapper />
+      </Suspense>
+      <Suspense fallback={<CardSkeleton />}>
+        <RunnersWrapper />
+      </Suspense>
+    </div>
+  </main>
+)
 
 export default Dashboard
